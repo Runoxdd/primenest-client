13f30112998d7273a57592532fc73fe047f8236f
Runoxdd <runoefekemo@gmail.com>
2026-02-07 16:53:22 +0100

    fix: point apiRequest to render production url

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   // This will look for a variable in Vercel, otherwise use localhost
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8800/api",
+  baseURL: "https://primenest-client.vercel.app/",
   withCredentials: true,
 });
 
