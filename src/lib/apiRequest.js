508353fc16d8f157453b1c9dd9451cdeded4b48f
Runoxdd <runoefekemo@gmail.com>
2026-02-07 17:13:51 +0100

    fix: point apiRequest to render backend instead of vercel

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   // This will look for a variable in Vercel, otherwise use localhost
-  baseURL: "https://primenest-client.vercel.app/",
+  baseURL: "https://primenest-api-vus0.onrender.com/api",
   withCredentials: true,
 });
 
