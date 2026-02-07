fc2dde570a7675984c9b4f9d50adec090f8e8fb9
Runoxdd <runoefekemo@gmail.com>
2026-02-07 17:55:23 +0100

    fix: hardcode working api url for mobile

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  // This will look for a variable in Vercel, otherwise use localhost
-  baseURL: "https://primenest-api-vus0.onrender.com/api",
+  baseURL: "https://primenest-api-vus0.onrender.com/api", 
   withCredentials: true,
 });
 
